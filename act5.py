1d6406cb49800b442f7aed32c83c7d2ed3254833
Dennys <dennys028ss@gmail.com>
2025-07-22 17:14:10 -0600

    programa casi terminado

--- act5.py
+++ act5.py
@@ -1,3 +1,8 @@
+ventas = []
+sumaventas = 0
+promventas = 0
+contdias = 0
+
 while True:
     print("Menu Principal")
     print("1. Ingresar ventas")
@@ -5,39 +10,66 @@ while True:
     print("3. Calcular venta mas alta y mas baja")
     print("4. Calcular promedio de ventas")
     print("5. Dias que superaron los Q1000")
-    print("6. Buscar venta especifica")
-    print("7. Calcular cada venta")
-    print("8. Salir")
+    print("6. Calcular cada venta")
+    print("7. Salir")
 
     opcion = input("Seleccione una opcion del menu: ")
-    ventas = []
-    sumaventas = 0
-    promventas = 0
-    contdias = 0
-
 
     match opcion:
         case "1":
             veces = int(input("Ingrese cuantas ventas quiere ingresar: "))
             for i in range(0, veces):
                 venta = int(input("Ingrese venta: "))
-                ventas.append(venta)
+                if venta >= 0:
+                    ventas.append(venta)
+                elif venta < 0:
+                    print("No puede ingresar numeros negativos a las ventas")
         case "2":
             print(f"Ventas ingresadas: {ventas}")
         case "3":
-            #print("Venta mas alta: ")
-            for j in ventas:
+            if not ventas:
+                print("No hay nada en la lista de ventas")
+            else:
+                vmin = vmax = ventas[0]
 
+                for num in ventas[1:]:
+                    if num > vmax:
+                        vmax = num
+                    elif num < vmin:
+                        vmin = num
+                    print(f"El valor mas alto de la lista es:{vmax}")
+                    print(f"El valor mas bajo de la lista es:{vmin}")
         case "4":
-            print("Promedio de ventas: ")
+            sumaventas = sum(ventas)
+            promventas = sumaventas/len(ventas)
+            print(f"El promedio de las ventas es: {promventas}")
+
         case "5":
-            print("Dias que superaron los Q1000: ")
+            sumaventas = sum(ventas)
+            if sumaventas >= 1000:
+                contdias += 1
+
+            print(f"La cantidad de dias qus superaron los Q1000 fueron: {contdias}")
+
         case "6":
-            print("Buscar venta especifica: ")
+            print(ventas)
+            for h in ventas:
+                print(f"Venta: {h}")
+                if h > 1000:
+                    print("Es una venta alta")
+                elif 500 <= h <= 1000:
+                    print("Es una venta media")
+                elif h < 500:
+                    print("Es una venta baja")
+
         case "7":
-            print("Calcular cada venta")
-        case "8":
-            print("Saliendo......")
+            print("Saliendo el programa.......")
+            print("Gracias por su visita :)")
             break
+
         case _:
-            print("Opcion invalida, vuelva a intentarlo")
+            print("Opcion incorrecta, vuelva a intentarlo")
+
+
+
+
